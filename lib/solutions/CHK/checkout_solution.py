a4c1eb512d5c3fd69c510e5df547accc15f83ca9
sbx_user1051 <sbx_user1051@169.254.112.53>
2024-06-30 20:24:54 +0000

    Sun Jun 30 20:24:54 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -48,14 +48,14 @@ def checkout(skus):
         'A': [(5, 200), (3, 130)],
         'B': [(2, 45)],
         'E': (2, 'B'),  # Buy 2 E, get one B free
-        'F': (3, 20),   # Buy 3 F, pay for 2
+        'F': [(3, 20)],   # Buy 3 F, pay for 2
         'H': [(10, 80), (5, 45)],
         'K': [(2, 150)],
         'N': (3, 'M'),  # Buy 3 N, get one M free
         'P': [(5, 200)],
         'Q': [(3, 80)],
         'R': (3, 'Q'),  # Buy 3 R, get one Q free
-        'U': (4, 120),  # Buy 3 U, get one U free (effectively 4 for 120)
+        'U': [(4, 120)],  # Buy 3 U, get one U free (effectively 4 for 120)
         'V': [(3, 130), (2, 90)]
     }
     
