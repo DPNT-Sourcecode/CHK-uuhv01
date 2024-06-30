ce7320726b7d1c435d44686553da7d8c2f90fc60
sbx_user1051 <sbx_user1051@169.254.80.165>
2024-06-30 11:52:02 +0000

    Sun Jun 30 11:52:02 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,29 +1,26 @@
+# CHK_R2
+# ROUND 2 - More offers
+# The checkout feature is great and our supermarket is doing fine. Is time to think about growth.
+# Our marketing teams wants to experiment with new offer types and we should do our best to support them.
 
-
-# noinspection PyUnusedLocal
-# skus = unicode string
-
-# ROUND 1 - Our supermarket
-# The purpose of this challenge is to implement a supermarket checkout that calculates the total price of a number of items.
-
-# In a normal supermarket, things are identified using Stock Keeping Units, or SKUs. 
-# In our store, we'll use individual letters of the alphabet (A, B, C, and so on). 
-# Our goods are priced individually. In addition, some items are multi-priced: buy n of them, and they'll cost you y pounds. 
-# For example, item A might cost 50 pounds individually, but this week we have a special offer: 
-#  buy three As and they'll cost you 130.
+# We are going to sell a new item E.
+# Normally E costs 40, but if you buy 2 of Es you will get B free. How cool is that ? Multi-priced items also seemed to work well so we should have more of these.
 
 # Our price table and offers: 
-# +------+-------+----------------+
-# | Item | Price | Special offers |
-# +------+-------+----------------+
-# | A    | 50    | 3A for 130     |
-# | B    | 30    | 2B for 45      |
-# | C    | 20    |                |
-# | D    | 15    |                |
-# +------+-------+----------------+
+# +------+-------+------------------------+
+# | Item | Price | Special offers         |
+# +------+-------+------------------------+
+# | A    | 50    | 3A for 130, 5A for 200 |
+# | B    | 30    | 2B for 45              |
+# | C    | 20    |                        |
+# | D    | 15    |                        |
+# | E    | 40    | 2E get one B free      |
+# +------+-------+------------------------+
 
 
 # Notes: 
+#  - The policy of the supermarket is to always favor the customer when applying special offers.
+#  - All the offers are well balanced so that they can be safely combined.
 #  - For any illegal input return -1
 
 # In order to complete the round you need to implement the following method:
@@ -32,7 +29,6 @@
 # Where:
 #  - param[0] = a String containing the SKUs of all the products in the basket
 #  - @return = an Integer representing the total checkout value of the items 
-# skus = 'AAABBCCAA'
 
 
 def checkout(skus):
@@ -40,9 +36,9 @@ def checkout(skus):
     skus = list(skus)
 
     # create a dictionary to store the prices of each letter
-    prices = {'A': 50, 'B': 30, 'C': 20, 'D':15}
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D':15, 'E':50}
     # create a dictionary to store the special offers and prices
-    offers = {'A': (3,130), 'B': (2,45)}
+    offers = {'A': [(3,130),(5,200)], 'B': (2,45), 'E':(2,'B')}
     # create a dictionary to count occurences of each letter
     # group letters if deal
     basket_ordered = {}
@@ -56,16 +52,19 @@ def checkout(skus):
     # apply bulk discount where possible
     total = 0
     print (basket_ordered)
+    # regular offer
     for sku, quantity in basket_ordered.items():
         if sku in offers:
             offer_req, offer_tot = offers[sku]
             total += (quantity // offer_req)*offer_tot
             total += (quantity%offer_req) * prices[sku]
+    # BOGOF offer
         else:
             total += quantity*prices[sku]
     print (total)
     return total
     
     
+
 
 
